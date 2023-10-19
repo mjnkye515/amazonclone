50750751dd0533673d189653fcd082ec54a37c53
Minkye <minh050105@gmail.com>
2023-10-19 20:15:22 +0700

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,8 @@
-export let cart = [{
-  productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-},{
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1,
-}, 
-];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function savetoStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtoCart(productId){
 
@@ -25,10 +22,12 @@ export function addtoCart(productId){
       quantity:1
     });
   }
+
+  savetoStorage();
 }
 
 export function removeFromCart(productId){
-  
+
   const newCart =[];
 
   cart.forEach((cartItem)=>{
@@ -36,5 +35,8 @@ export function removeFromCart(productId){
       newCart.push(cartItem);
     }
   });
+
   cart = newCart;
+
+  savetoStorage();
 }
