026a201df580144837ba4db2f62d551de563da2c
Minkye <minh050105@gmail.com>
2023-10-19 19:58:16 +0700

    Delete porducts from HTML

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,7 @@ import { fomatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML ='';
 
-cart.forEach((cartItem, index)=>{
+cart.forEach((cartItem)=>{
 
   const {productId}= cartItem;
 
@@ -17,7 +17,8 @@ cart.forEach((cartItem, index)=>{
   });
 
   cartSummaryHTML +=`
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -100,5 +101,9 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
-})
+});
+
+
